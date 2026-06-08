1634afc730ce5135fbb08a67ccbe149f6f5a847c
Shru-ti-00 <shrutimunda085@gmail.com>
2026-06-08 23:54:15 +0530

    added java program for basic operation

---
+++ BasicDemo.java
@@ -0,0 +1,16 @@
+public class BasicDemo 
+{ 
+    public static void main(String[] args) 
+    { 
+        int a = 10, b = 5; 
+        System.out.println("Sum: " + (a + b)); 
+   
+        if(a > b) 
+            System.out.println("a is greater than b"); 
+        
+        int[] arr = {1,2,3,4,5}; 
+    
+        for(int i : arr) 
+            System.out.print(i + " "); 
+    } 
+} 
