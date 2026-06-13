fc81762de769737b9cc6b98051774edb7e25a4d2
Shru-ti-00 <shrutimunda085@gmail.com>
2026-06-13 22:12:14 +0530

    added java program to demonstrate inheritance

---
+++ InheritDemo.java
@@ -0,0 +1,23 @@
+class Parent
+{ 
+    void show() { 
+        System.out.println("Parent method"); 
+    } 
+} 
+
+class Child extends Parent 
+{ 
+    void show() { 
+        super.show(); 
+        System.out.println("Child method"); 
+    } 
+} 
+
+public class InheritDemo 
+{ 
+    public static void main(String[] args) 
+    { 
+        Child c = new Child(); 
+        c.show(); 
+    } 
+}
